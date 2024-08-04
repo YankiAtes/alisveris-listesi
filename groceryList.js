4f1faa30bdc5a69c9785b5f11dc7eea0f36bcf0c
Yankı Ateş <derinyankiates88@gmail.com>
2024-08-04 22:01:46 +0300

    db 1

--- groceryList.js
+++ groceryList.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHmrsv_dUf7EoypSDZtGp9WsjWRVK4pXQ",
+  authDomain: "alisveris-listesi-3b448.firebaseapp.com",
+  projectId: "alisveris-listesi-3b448",
+  storageBucket: "alisveris-listesi-3b448.appspot.com",
+  messagingSenderId: "88875285131",
+  appId: "1:88875285131:web:80aef906fad7b63509328e",
+  measurementId: "G-MPHE5C4PT4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 let grocery = document.getElementById("grocery");
 grocery.addEventListener("submit", addItem)
 
@@ -22,4 +45,5 @@ function addItem(e){
 function deleteItem(e){
     this.parentElement.remove();
 }
+
 
