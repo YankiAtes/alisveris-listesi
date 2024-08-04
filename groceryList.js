9985d7b6d279dba8048ba011320fece0ef46f298
Yankı Ateş <derinyankiates88@gmail.com>
2024-08-04 21:45:38 +0300

    ana yapı tamam
    database olmadan ve kalıcı olarak kayıt etmeden ana sistem çalışıyor

---
+++ groceryList.js
@@ -0,0 +1,25 @@
+let grocery = document.getElementById("grocery");
+grocery.addEventListener("submit", addItem)
+
+function addItem(e){
+    e.preventDefault();
+    let data = this.elements.writeList.value;
+    let list = document.querySelector("ol");
+    let item = document.createElement("li");
+    let text = document.createElement("p")
+
+    text.textContent = data;
+    this.elements.writeList.value = "";
+    item.append(text);
+    list.append(item);
+
+    let removeButton = document.createElement("span");
+    removeButton.classList.add("remove");
+    item.append(removeButton);
+    removeButton.addEventListener("click", deleteItem);
+}
+
+function deleteItem(e){
+    this.parentElement.remove();
+}
+
