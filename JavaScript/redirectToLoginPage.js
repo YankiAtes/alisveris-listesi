55e91c42377845596ced187d7e3fd89137c96f30
Yankı Ateş <derinyankiates88@gmail.com>
2024-08-09 10:40:37 +0300

    redirection complete

--- JavaScript/redirectToLoginPage.js
+++ JavaScript/redirectToLoginPage.js
@@ -1,5 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHmrsv_dUf7EoypSDZtGp9WsjWRVK4pXQ",
+  authDomain: "alisveris-listesi-3b448.firebaseapp.com",
+  projectId: "alisveris-listesi-3b448",
+  storageBucket: "alisveris-listesi-3b448.appspot.com",
+  messagingSenderId: "88875285131",
+  appId: "1:88875285131:web:80aef906fad7b63509328e",
+  measurementId: "G-MPHE5C4PT4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+let reference = doc(db, "data", "forceLogin");
+let docSnap = await getDoc(reference);
 //Redirect to login page on page load
-if (localStorage.getItem("isUserSignedIn") === 0) {
+if (localStorage.getItem("isUserSignedIn") == 0 && docSnap.data().value == 1) {
   const meta = document.createElement("meta");
   meta.setAttribute("http-equiv", "refresh");
   meta.setAttribute("content", '0; URL="SignIn.html"');
