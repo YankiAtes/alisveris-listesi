b838aba08cd1edaee834e59d57f5a766e7e8185b
Yankı Ateş <derinyankiates88@gmail.com>
2024-08-09 10:13:47 +0300

    redirection

--- JavaScript/authentication.js
+++ JavaScript/authentication.js
@@ -117,6 +117,7 @@ onAuthStateChanged(auth, (user) => {
 
     //Redirect to login page on page load
     const meta = document.createElement("meta");
+    meta.setAttribute("http-equiv", "refresh");
     meta.setAttribute("content", '0; URL="SignIn.html"');
     document.head.appendChild(meta);
   }
